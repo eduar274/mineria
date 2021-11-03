c9b52e2474db5ad02fa1e8a80cc3277e5de6d3c2
eduar274 <43424394+eduar274@users.noreply.github.com>
2021-11-03 13:58:49 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import  requests as rq
+#from flask import Flask,jsonify
+import json
+
+def get_tendencias():
+    trends = json.loads(rq.get('https://api.mercadolibre.com/trends/MCO').content)
+    for trend in trends:
+        searches = json.loads(rq.get(f"https://api.mercadolibre.com/sites/MCO/search?q={trend['keyword'].replace(' ','%20')}").content)
+        #print(searches)
+        for search in searches['results']:
+            results = search['id'].strip() 
+            #print(results)
+            items = json.loads(rq.get(f"https://api.mercadolibre.com/items/{results}").content)
+            #print(results, items['catalog_product_id'])
+            item_id = items['catalog_product_id']
+            if not item_id == None: 
+                rewis = json.loads(rq.get(f"https://api.mercadolibre.com/reviews/item/{item_id}").content)
+                #print(rewis)
+                if rewis['rating_average'] > 0.0:
+                    print(rewis)
+            
+        
+    #print(trends)
+
+  
+
+
+if __name__=="__main__":
+    get_tendencias()
