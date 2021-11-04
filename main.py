3f0030c2cb28c8b64a10d975fabc474de9629d27
Riteluit <camiloarmandorojas@gmail.com>
2021-11-04 00:29:22 -0500

    Mysql

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import  requests as rq
 #from flask import Flask,jsonify
 import json
 import mysql.connector
-import time
+
+def Connection():
+    conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="",database="mineria")
+    cursor1 = conexion1.cursor()
+
 
 
 def get_tendencias():
     trends = json.loads(rq.get('https://api.mercadolibre.com/trends/MCO').content)
     for trend in trends:
         searches = json.loads(rq.get(f"https://api.mercadolibre.com/sites/MCO/search?q={trend['keyword'].replace(' ','%20')}").content)
-        #print(searches)
+        #print(searches[])
         for search in searches['results']:
             results = search['id'].strip() 
             #print(results)
@@ -22,28 +26,53 @@ def get_tendencias():
                 if reviews['rating_average'] > 0.0:
                     #print(reviews['reviews'])
                     for review in reviews['reviews']:
-                        print(review['content'])
-                    
-            
-        
-    #print(trends)
+                        #print(review['content'])
+                        mysql_insertArticulos(item_id,items['title'],items['price'],reviews['rating_average'])
+                        mysql_insertReview(review['id'],review['title'],review['content'],reviews['rating_average'],review['date_created'],item_id)
 
-  
-def mysql_Connector():
-    conexion1=mysql.connector.connect(host="localhost", user="root", passwd="",database="mineria")
-    cursor1=conexion1.cursor()
-    cursor1.execute("show tables")
-    for tabla in cursor1:
-        print(tabla)
+
+def mysql_insertReview(id,title,content,rating,date,item_id):
+    #Open Connection
+    conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="",database="mineria")
+    cursor1 = conexion1.cursor()
+    
+    select  = "select * from reviews where id = %s"
+    id_where = (id,)
+    cursor1.execute(select, id_where)
+    
+    art = cursor1.fetchall()
+    print(art)
+
+    if (len(art) == 0 ):
+        sql = "insert into reviews(id, titulo, comentario, rating_avg, fecha, id_articulo) values (%s,%s,%s,%s,%s,%s)"
+        datos = (id,title,content,rating,date,item_id,)
+        cursor1.execute(sql, datos)
+        conexion1.commit()
+
+    
+    conexion1.close() 
+
+
+def mysql_insertArticulos(id,title,price,rating_average):
+    #Open Connection
+    conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="",database="mineria")
+    cursor1 = conexion1.cursor()
+
+    select  = "select * from articulos where id = %s"
+    id_where = (id,)
+    cursor1.execute(select, id_where)
+    
+    art = cursor1.fetchall()
+    #print(art)
+    if (len(art) == 0 ):
+        #print("HOLA ENTRE!!!")
+        insert = "insert into articulos(id, nombre_categoria, precio_avg, rating_avg) values (%s,%s,%s,%s)"
+        datos = (id, title, price, rating_average,)       
+        cursor1.execute(insert, datos)
+        conexion1.commit()
+
+    
     conexion1.close()
-
-def mysql_insertReview(nombre_Categoria_iD, nombre_Categoria , producto, rating):
-
-
-def mysql_insertArticulos(Comentario):
-
-
-
-
+         
 if __name__=="__main__":
     get_tendencias()
